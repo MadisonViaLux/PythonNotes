51e9bc8780e99eeae222c5f05337ecc9a97960da
MadisonViaLux <madison.s.mckown@gmail.com>
2020-08-04 15:56:44 -0700

    spacing

--- Hacker_Rank_Notes/HackerRankNotes.py
+++ Hacker_Rank_Notes/HackerRankNotes.py
@@ -420,8 +420,6 @@
 # # print(threeNumberSum([12, 3, 1, 2, -6, 5, -8, 6], 0)) #<---------- [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
 
 
-
-
 def threeNumberSum(arr, target):
     new_list = set()
     for i in arr:
